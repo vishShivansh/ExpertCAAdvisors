bd3fa4aaf11844bf519b8b6b5c272561931f5c6a
Shivansh <sgshivansh22@gmail.com>
2024-07-06 16:55:58 +0530

    ream section made responsive

--- client/src/components/TeamSection.jsx
+++ client/src/components/TeamSection.jsx
@@ -1,11 +1,11 @@
 const TeamSection = () => {
   return (
     <section className="flex items-center justify-center h-[500px] bg-white">
-      <div className="w-3/4">
+      <div className="w-3/4 2sm:w-[400px]">
         <div className="text-center">
           <div className="max-w-2xl mx-auto">
             <h2 className="text-4xl font-bold mb-4">Our Team</h2>
-            <p className="text-lg text-gray-70">
+            <p className="text-lg text-gray-70 2sm:line-clamp-10">
               Our team is composed of highly qualified and experienced chartered
               accountants and financial professionals. Each member of our team
               brings a wealth of knowledge and expertise, ensuring that we can
