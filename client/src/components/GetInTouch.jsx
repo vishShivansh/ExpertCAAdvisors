0b1769957f6876bdf95060d9c6eadc3ef2406af9
Shivansh <sgshivansh22@gmail.com>
2024-07-09 00:30:58 +0530

    same as latter

--- client/src/components/GetInTouch.jsx
+++ client/src/components/GetInTouch.jsx
@@ -20,7 +20,7 @@ const GetInTouch = () => {
     event.preventDefault();
 
     try {
-      const response = await fetch("https://localhost:5000", {
+      const response = await fetch("http://localhost:5000/send-email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
